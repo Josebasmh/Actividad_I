49271550b9e68b518181bc4d36e1a930b8c7157c
Joseba <117350714+Josebasmh@users.noreply.github.com>
2023-11-03 13:45:59 +0100

    Squashed commit of the following:
    commit e542eef4780ccc45db6cc4b1dacaaec216f45724
    Author: Joseba <117350714+Josebasmh@users.noreply.github.com>
    Date:   Fri Nov 3 13:45:36 2023 +0100
    
        FIX-traducciones
    
        Finalizado el apartado de las traducciones
    
    commit 841cc7267e22d0af16f3a78454e61d1b5bd6b048
    Author: Joseba <117350714+Josebasmh@users.noreply.github.com>
    Date:   Thu Nov 2 13:24:48 2023 +0100
    
        ADD-fxml traducciones(sin terminar)
    
    commit 4bca5767b0c1633b111b5636156fb4c38b77f954
    Author: Joseba <117350714+Josebasmh@users.noreply.github.com>
    Date:   Thu Nov 2 10:00:51 2023 +0100
    
        ADD-idiomas y MOD Main.java
    
        Añadidos los idiomas y configurado el Main para que antes de que inicie la ventana lea el idioma y cargue los archivos. falta en el fxml insertar las variables en el texto Ejemplo: %nombre

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,24 +1,28 @@
 package application;
 	
 import java.io.IOException;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import model.Propiedades;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.scene.layout.FlowPane;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage stage) {
-		stage.setTitle("PERSONA");
 		
-		//Image imgLogo = new Image();
 		FlowPane root;
 		try {
-			root = (FlowPane)FXMLLoader.load(getClass().getResource("/fxml/Persona.fxml"));
+			String idioma = Propiedades.getValor("idioma");
+			String region = Propiedades.getValor("region");
+			ResourceBundle bundle= ResourceBundle.getBundle("idiomas/messages",new Locale(idioma,region));
+			root = (FlowPane)FXMLLoader.load(getClass().getResource("/fxml/Persona.fxml"),bundle);
+			stage.setTitle(bundle.getString("title"));
 			Scene scene = new Scene(root,820,620);
 			stage.setScene(scene);
 			stage.setMinHeight(620);
